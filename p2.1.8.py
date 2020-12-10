1fc881014765daf55bf0132a4f857232f44ab7a2
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2020-12-10 15:06:31 +0700

    p2.1.8

---
+++ p2.1.8.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from base.sinusoidal import *
+
+begin = 0
+end = 100
+
+x1, n = sin(1, np.pi * 0.1, 0, begin, end)
+x2 = np.exp(0.01 * n)
+x = np.multiply(x1, x2)
+
+# x, n
+plt.plot(n,x)
+plt.xlim(begin, end)
+plt.xlabel('n')
+plt.ylabel('x')
+plt.title('x(n)')
+
+plt.show()

d237cc2efed692a79b4b77d1d4e93f0b177b8631
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2020-12-12 21:16:40 +0700

    remove rem

--- base/periodic.py
+++ base/periodic.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function, unicode_literals
 import numpy as np
 
 # Generates xtilde from x in P periods;
-def periodic(x, P):    
+def periodic(x, P):
     xtilde = x
     for _ in range(P-1):
         xtilde = np.concatenate((xtilde, x))

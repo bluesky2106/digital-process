6da26693b5d32b2b98912da0bbd6adb6cbc08715
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2020-12-13 09:35:03 +0700

    p2.1.2

--- p2.1.2.py
+++ p2.1.2.py
@@ -3,30 +3,17 @@ import numpy as np
 from base.impseq import *
 from base.conv_m import *
 
-delta, m = impseg(0, -10, 10)
-h = np.exp(0.5 * m)
-x, n = conv_m(delta, m, h, m)
+n = np.arange(-10, 11)
+x = np.zeros(len(n))
+j = 0
+for i in n:
+    if i % 2 == 0:
+        x[j] = np.exp(-0.5 * abs(i/2))
+    j += 1
 
-# delta, m
-plt.subplot(2, 2, 1)    
-plt.bar(m,delta, width = 0.1)
-plt.xticks(range(np.amin(m), np.amax(m)))
-plt.xlabel('m')
-plt.ylabel('delta')
-plt.title('delta')
 
-# h, m
-plt.subplot(2, 2, 2)    
-plt.bar(m,h, width = 0.1)
-plt.xticks(range(np.amin(m), np.amax(m)+1))
-plt.xlabel('m')
-plt.ylabel('h')
-plt.title('h(m)')
-
-plt.subplot(2, 2, 3)
 plt.bar(n,x, width = 0.1)
-plt.xticks(range(np.amin(m), np.amax(m)+1))
-plt.xlim(np.amin(m), np.amax(m)+1)
+plt.xticks(range(np.amin(n), np.amax(n)+1))
 plt.xlabel('n')
 plt.ylabel('x')
 plt.title('x(n)')

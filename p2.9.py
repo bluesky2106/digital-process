9b765332e3b40874c80f7517eba772c804cce881
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2020-12-13 11:00:01 +0700

    p2.9

--- p2.9.py
+++ p2.9.py
@@ -1,21 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from base.conv_m import *
+from base.sigfold import *
 
-n = np.arange(0,21)
-x = np.power(0.9, n)
+nx = np.arange(0,21)
+x = np.power(0.9, nx)
+x_flip, nx_flip = sigfold(x, nx)
 
-y = np.power(0.8, -1*n)
+y, ny = sigfold(np.power(0.8, nx), nx)
 
-rxx, n_rxx = conv_m(x, n, x, n)
-rxy, n_rxy = conv_m(x, n, y, n)
+rxx, n_rxx = conv_m(x, nx, x_flip, nx_flip)
+rxy, n_rxy = conv_m(x, nx, y, ny)
 
-# x, n
+# x, nx
 plt.subplot(2, 2, 1)    
-plt.bar(n, x, width = 0.1)
-plt.xlabel('n')
+plt.bar(nx, x, width = 0.1)
+plt.xlabel('nx')
 plt.ylabel('x')
-plt.title('x(n)')
+plt.title('x(nx)')
 
 # rxx, n_rxx
 plt.subplot(2, 2, 2)    
@@ -24,12 +26,12 @@ plt.xlabel('n_rxx')
 plt.ylabel('rxx')
 plt.title('autocorrelation')
 
-# y, n
+# y, ny
 plt.subplot(2, 2, 3)    
-plt.bar(n, y, width = 0.1)
-plt.xlabel('n')
+plt.bar(ny, y, width = 0.1)
+plt.xlabel('ny')
 plt.ylabel('y')
-plt.title('y(n)')
+plt.title('y(nx)')
 
 # rxy, n_rxy
 plt.subplot(2, 2, 4)    
